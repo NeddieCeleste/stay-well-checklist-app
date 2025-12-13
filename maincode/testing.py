6fda2083ede056d6a6e8529359cca8e094c94c51
Neddie Celeste <neddieceleste@gmail.com>
2025-12-14 01:17:18 +0200

    updated main functionality
    Cleaner code, better functions, set stuff in folders and began testing PyQt.

--- maincode/testing.py
+++ maincode/testing.py
@@ -14,7 +14,7 @@ def help_list():
 
 def load_tasks():
     checklist_data = []
-    filename = "output.json"
+    filename = "../data/output.json"
     if not os.path.exists(filename):
         # File does not exist → return empty list
         return checklist_data
@@ -24,46 +24,44 @@ def load_tasks():
         # File is empty → return empty list
         return checklist_data
 
-    with open("output.json", "r") as outfile:
+    with open("../data/output.json", "r") as outfile:
         return json.load(outfile)
 
-def create_new_checklist():
-    global main_list, checklist_contents, checklist, new_checklist, open_checklist, checklist_name
-
-    open_checklist = True
+def create_new_checklist(main_list, checklist_contents, checklist_name):
     if checklist_contents:
         new_checklist = {"Name": checklist_name, "Checklist": checklist_contents.copy()}
         main_list.append(new_checklist)
         checklist_contents = []
     checklist_name = input("Please enter the name of the new checklist: ")
+    return main_list, checklist_contents, checklist_name
 
-def add_new_entry():
-    global checklist_contents
+def add_new_entry(checklist_contents):
     to_do_input = input("What would you like to add?: ")
     checklist_contents.append(to_do_input)
+    return checklist_contents
 
-def show_all_checklists():
+def show_all_checklists(main_list):
     for checklists in main_list:
         print(f"{checklists["Name"]}:\n* {'\n* '.join(checklists['Checklist'])}\n")
 
-def remove_checklist():
-    global main_list, command
+def remove_checklist(main_list, command):
     index = command.split()[2:]
     index_together = ' '.join(index)
     for checklists in main_list:
         if checklists["Name"] == index_together:
             main_list.remove(checklists)
+    return main_list
 
-def save_changes():
-    global main_list, new_checklist, checklist_contents
+def save_changes(main_list, checklist_contents, checklist_name):
     if checklist_contents:
         new_checklist = ({"Name": checklist_name, "Checklist": checklist_contents.copy()})
         main_list.append(new_checklist)
-    with open("output.json", "w") as outfile:
+    with open("../data/output.json", "w") as outfile:
         json.dump(main_list, outfile, indent="")
+    return main_list
 
-def show_specific_checklist():
-    global selected_checklist, checklist, found_checklist, main_list
+def show_specific_checklist(main_list, command):
+    found_checklist = False
     selected_checklist = ' '.join(command.split()[1:])
     for checklist in main_list:
         if selected_checklist in checklist["Name"]:
@@ -71,10 +69,9 @@ def show_specific_checklist():
             print(f"{checklist["Name"]}:\n* {'\n* '.join(checklist['Checklist'])}\n")
     if not found_checklist:
         print("No checklists found, please try again.")
-    found_checklist = False
 
-def edit_checklist():
-    global selected_checklist, checklist, main_list, found_checklist
+def edit_checklist(command, main_list):
+    found_checklist = False
     selected_checklist = ' '.join(command.split()[2:])
     for checklist in main_list:
         if selected_checklist in checklist["Name"]:
@@ -106,44 +103,49 @@ def edit_checklist():
                     print("Invalid command.")
     if not found_checklist:
         print("No checklists found, please try again.")
-    found_checklist = False
+    return main_list
 
 
 main_list = load_tasks()
 checklist_contents = []
 open_checklist = False
-found_checklist = False
+checklist_name = ""
 while True:
 
     command = input("What would you like to do?(type help for commands): ")
 
     if command == "exit":
-        save_changes()
+        main_list = save_changes(main_list, checklist_contents, checklist_name)
         break
 
     elif command == "help":
         help_list()
 
     elif command == "new checklist":
-        create_new_checklist()
+        open_checklist = True
+        main_list, checklist_contents, checklist_name = create_new_checklist(
+            main_list,
+            checklist_contents,
+            checklist_name)
 
     elif command == "add entry" and open_checklist:
-        add_new_entry()
+        checklist_contents = add_new_entry(checklist_contents)
 
     elif "remove checklist" in command:
-        remove_checklist()
+        main_list = remove_checklist(main_list, command)
 
     elif command == "show all":
-        show_all_checklists()
+        show_all_checklists(main_list)
 
     elif "show" in command and len(command.split()) >= 2:
-        show_specific_checklist()
+        show_specific_checklist(main_list, command)
+        found_checklist = False
 
     elif "edit checklist" in command:
-        edit_checklist()
+        main_list = edit_checklist(command, main_list)
 
     elif command == "save":
-        save_changes()
+        main_list = save_changes(main_list, checklist_contents, checklist_name)
 
     else:
         print("You probably made a mistake in your input! Please, try again.")
